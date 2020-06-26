f75ef6f3e2526d89fbbdad3c4dc2bfb8cafcb814
lauren <ljdutton2@icloud.com>
2020-06-26 14:28:39 -0700

    subreddits

--- controllers/posts.js
+++ controllers/posts.js
@@ -41,4 +41,15 @@ module.exports = (app) => {
       });
   });
 
+  // SUBREDDIT
+  app.get("/n/:subreddit", function(req, res) {
+    Post.find({ subreddit: req.params.subreddit }).lean()
+      .then(posts => {
+        res.render("posts-index", { posts });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+
 };
