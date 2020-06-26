53e0db26a43b06cb130b75e13b073d31cf9b9d30
lauren <ljdutton2@icloud.com>
2020-06-26 16:47:19 -0700

    signup

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
-
+var cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+app.use(cookieParser()); // Add this after you initialize express.
 
 // require handlebars
 const exphbs = require('express-handlebars');
@@ -22,8 +24,12 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 // Use handlebars to render
 app.set('view engine', 'handlebars');
 
+
+require('dotenv').config();
 require('./controllers/posts.js')(app);
 require('./controllers/comments.js')(app);
+require('./controllers/auth.js')(app);
+
 
 // NEW
 // app.get('/posts/new', (req, res) => res.render('posts-new'))
