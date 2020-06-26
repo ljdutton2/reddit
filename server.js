eb030a3f1469b78b2f969acf623ce3879aa41c22
lauren <ljdutton2@icloud.com>
2020-06-26 11:57:14 -0700

    form

--- server.js
+++ server.js
@@ -5,15 +5,16 @@ const port = 3000
 // require handlebars
 const exphbs = require('express-handlebars');
 
+
+require('./controllers/posts.js')(app);
 // Use "main" as our default layout
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 // Use handlebars to render
 app.set('view engine', 'handlebars');
 
-// Render the "home" layout for the main page and send the following msg
-app.get('/', (req, res) => {
-  res.render('home', { msg: 'Handlebars are Cool!' });
+// NEW
+app.get('/posts/new', (req, res) => {
+  res.render('posts-new', { posts: posts});
 })
-
 
 app.listen(port, () => console.log(` app listening at http://localhost:${port}`))
